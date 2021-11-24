51ed76c5725e8898a488be87b265d0902cfb3ed7
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-11-24 17:00:10 +0300

    without changing anything

--- csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
+++ csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SumAverageRunningInt
 {
@@ -7,13 +11,14 @@ namespace SumAverageRunningInt
         static void Main(string[] args)
         {
             var sum = 0;
-            int average;
+            
             const int lowerBound = 1;
             const int upperBound = 100;
 
             for (var number = lowerBound; number <= upperBound; ++number) 
             {
                 sum += number;
+                Console.WriteLine(sum);
             }
         }
     }
