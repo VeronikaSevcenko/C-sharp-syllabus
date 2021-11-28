20e88f0b8339d5e93317519693bb2da3b016ef9f
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-11-28 23:17:47 +0300

    Exercise 4 in FlowControl folder (if)

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/PrintDayWord2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PrintDayWord2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayNumber = int.Parse(Console.ReadLine());
+
+            if (dayNumber == 1)
+            {
+                Console.WriteLine("Monday");
+
+            } if (dayNumber == 2)
+            {
+                Console.WriteLine("Tuesday");
+
+            } if (dayNumber == 3)
+            {
+                Console.WriteLine("Wednesday");
+            }
+            if (dayNumber == 4)
+            {
+                Console.WriteLine("Thursday");
+            }
+            if (dayNumber == 5)
+            {
+                Console.WriteLine("Friday");
+            }
+            if (dayNumber == 6)
+            {
+                Console.WriteLine("Saturday");
+            }
+            if (dayNumber == 7)
+            {
+                Console.WriteLine("Sunday");
+            } 
+            if ( dayNumber > 8)
+            {
+                Console.WriteLine("Not a valid day");
+            }
+        }
+    }
+}
