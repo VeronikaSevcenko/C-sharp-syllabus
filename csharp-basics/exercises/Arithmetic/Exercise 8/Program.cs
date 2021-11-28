6a4d2b30881a58f0d33615a8a7b8aeef8e44b03a
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-11-28 20:25:55 +0300

    Exercise 8 in Arithmetics folder (changed)

--- csharp-basics/exercises/Arithmetic/Exercise 8/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise 8/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Exercise_8
 {
@@ -6,35 +10,49 @@ namespace Exercise_8
     {
         static void Main(string[] args)
         {
-            
-
-
-
-
-            double minWage = 8.0;
-            int maxHours = 60;
             double basePay = 0;
+            double minBasePay = 8.0;
             int hoursWorked = 0;
-            double totalSalary = 0;
+            int maxHour = 60;
 
-            if ((basePay < minWage) || (hoursWorked > maxHours))
+            for (int i = 1; i <= 3; i++)
             {
-                Console.WriteLine("Error");
+                Console.WriteLine("Employee Number" + i);
 
-            } else if (hoursWorked > 40)
-            {
-                totalSalary = basePay * 40 + 1.5 * basePay * (hoursWorked - 40);
+                Console.WriteLine("Enter Base Pay");
+                basePay = double.Parse(Console.ReadLine());
 
-            } else
+                Console.WriteLine("Enter Hours Worked");
+                hoursWorked = int.Parse(Console.ReadLine());
 
-            {
-                totalSalary = basePay * hoursWorked;
+                double totalSalary = 0;
+                if ((basePay < minBasePay) || (hoursWorked > maxHour))
+                {
+                    Console.WriteLine("Error");
+                }
+                else if (hoursWorked > 40)
+                {
+                    totalSalary = basePay * 40 + 1.5 * basePay * (hoursWorked - 40);
+                }
+                else
+                {
+                    totalSalary = basePay * hoursWorked;
+                }
+                Console.WriteLine($"Total salary is : " + totalSalary + "$");
+
+                Console.ReadKey();
+
             }
-            Console.WriteLine("Your total salary is " + totalSalary);
-
-            Console.ReadKey();
-
-
         }
+        
+        
+            
+        
+
+
+
+
+
+
     }
 }
