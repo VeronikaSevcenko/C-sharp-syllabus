ac071447e1dd42a014420befd0f29a9954dece22
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-01 13:41:22 +0300

    correction exercise 3 in Types&Variables folder

--- csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
@@ -39,7 +39,6 @@ namespace Casting
             double d = 4.2;
             float e = 5.3f;
 
-            //fixme - should be 15.5 :| 
             float sum = float.Parse(a) + (float)b + (float)c + (float)d + e;
             Console.WriteLine(sum);
         }
